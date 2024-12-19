ef60d484529765df0e5990d2f420dd705d322253
Wayne <weifeng1827@gmail.com>
2024-12-19 18:41:22 +0800

    創建memberscreen&景點收藏以及收藏景點清單packge

--- app/src/main/java/com/example/tripapp/ui/feature/member/MemberNavigation.kt
+++ app/src/main/java/com/example/tripapp/ui/feature/member/MemberNavigation.kt
@@ -1,2 +1,18 @@
 package com.example.tripapp.ui.feature.member
 
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.example.tripapp.ui.feature.spending.list.SpendingRoute
+
+val SPENDING_MEMBER_ROUTE = "Member"
+
+fun genSpendingNavigationRoute() = SPENDING_MEMBER_ROUTE
+
+fun NavGraphBuilder.spendingListRoute(navController: NavHostController) {
+    composable(
+        route = SPENDING_MEMBER_ROUTE,
+    ) {
+        MemberRoute()
+    }
+}
