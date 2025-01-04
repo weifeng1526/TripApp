851c8a99726451af89e778a2a8e41c052034cdd6
Sue <a0938799886@gmail.com>
2025-01-04 12:19:59 +0800

    加了路徑

--- app/src/main/java/com/example/tripapp/ui/feature/map/MapNavigation.kt
+++ app/src/main/java/com/example/tripapp/ui/feature/map/MapNavigation.kt
@@ -1,2 +1,18 @@
 package com.example.tripapp.ui.feature.map
 
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.example.tripapp.ui.feature.member.login.MemberLoginRoute
+
+val MAP_ROUTE = "map"
+
+fun genMapNavigationRoute() = MAP_ROUTE
+
+fun NavGraphBuilder.MapRoute(navController: NavHostController) {
+    composable(
+        route = MAP_ROUTE,
+    ) {
+        MemberLoginRoute(navController)
+    }
+}
