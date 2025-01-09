5908b4e501e5b9cb3ca3cf176f0d76833f6eb357
Jonas Lin <abcbefg42@gmail.com>
2025-01-09 21:15:47 +0800

    VM

--- app/src/main/java/com/example/tripapp/ui/feature/trip/notes/note/NotesViewModel.kt
+++ app/src/main/java/com/example/tripapp/ui/feature/trip/notes/note/NotesViewModel.kt
@@ -1,4 +1,42 @@
 package com.example.tripapp.ui.feature.trip.notes.note
 
-class NotesViewModel {
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.tripapp.ui.feature.trip.dataObjects.Notes
+import com.example.tripapp.ui.restful.RequestVM
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class NotesViewModel : ViewModel() {
+    val requestVM =  RequestVM()
+
+    private var _notesState = MutableStateFlow(Notes())
+    val notesState = _notesState.asStateFlow()
+
+    fun createNotes(notes: Notes) {
+        viewModelScope.launch {
+            val response = requestVM.CreateNotes(notes)
+            Log.d("createNotes", response.toString())
+        }
+    }
+
+    fun getNotes(id: Int) {
+        viewModelScope.launch {
+            val response = requestVM.GetNotes(id)
+            Log.d("getNotes", response.toString())
+            _notesState.update {
+                response!!
+            }
+        }
+    }
+
+    fun updateNotes(notes: Notes) {
+        viewModelScope.launch {
+            val response = requestVM.UpdateNotes(notes)
+            Log.d("updateNotes", response.toString())
+        }
+    }
 }
